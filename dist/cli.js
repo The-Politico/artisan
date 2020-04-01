2bf94a9306d411c36331be8496b2d7ae31778639
Andrew Briz <briz.andrew@gmail.com>
2020-04-01 13:57:06 -0400

    Forgot to build.

--- dist/cli.js
+++ dist/cli.js
@@ -3082,7 +3082,7 @@ function () {
 }());
 
 var name = "@politico/artisan";
-var version = "0.0.12";
+var version = "0.0.13";
 var description = "A suite of tools for creating & managing Adobe Illustrator based embeds.";
 var main = "dist/index.js";
 var module$1 = "dist/module.js";
