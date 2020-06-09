9d9235efc87e95c6ada66278e55a5f3d65c619e7
Andrew Briz <briz.andrew@gmail.com>
2020-06-09 17:49:39 -0400

    Don't need that in there

--- example/src/components/preview/EmbedPreview/icons/Tablet.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Tablet = (props) => (
-  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 90 62'>
-    <path d='M0,54a8,8,0,0,0,8,8H82a8,8,0,0,0,8-8V8a8,8,0,0,0-8-8H8A8,8,0,0,0,0,8Zm8,4V4H78V58ZM80,31a4,4,0,1,1,4,4A4,4,0,0,1,80,31Z' />
-  </svg>
-);
-
-export default Tablet;
--- example/src/components/preview/EmbedPreview/icons/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Desktop } from './Desktop';
-export { default as Full } from './Full';
-export { default as Mobile } from './Mobile';
-export { default as Tablet } from './Tablet';
