783410c48dd54e3a3249226770859985cf5ccb95
Aaron Albright <aalbright@politico.com>
2022-10-05 16:30:32 -0400

    illustration operation docs

--- src/utils/store/operations/illustration.js
+++ src/utils/store/operations/illustration.js
@@ -2,7 +2,15 @@ import { initStore } from '../init';
 
 const store = initStore();
 
-export async function addIllustration(projectSlug, illustrationName) {
+/**
+ * Adds an illustration to a project with `publicURL` set to `null`.
+ * @param {Object} opts
+ * @param {String} opts.projectSlug Kebab-case project slug name
+ * @param {String} opts.illustrationName Illustration display name
+ * (e.g. "Illustration Name")
+ * @returns
+ */
+export async function addIllustration({ projectSlug, illustrationName }) {
   const projectEntry = await store.get(projectSlug);
 
   if (projectEntry) {
@@ -25,6 +33,14 @@ export async function addIllustration(projectSlug, illustrationName) {
   return null;
 }
 
+/**
+ * Updatesa an illustration's public URL field.
+ * @param {String} projectSlug Kebab-case project slug name
+ * @param {String} illustrationSlug Kebe-case illustration slug name
+ * @param {String} publicURL Illustration's public URL once the project
+ * has been published
+ * @returns
+ */
 export async function updateIllustrationURL(
   projectSlug,
   illustrationSlug,
@@ -57,6 +73,12 @@ export async function updateIllustrationURL(
   throw new Error(`No such project exists for: ${projectSlug}`);
 }
 
+/**
+ * Removes an illustration from a project.
+ * @param {String} projectSlug Kebab-case project slug name
+ * @param {String} illustrationSlug Kebe-case illustration slug name
+ * @returns
+ */
 export async function removeIllustration(projectSlug, illustrationSlug) {
   const projectEntry = await store.get(projectSlug);
 
