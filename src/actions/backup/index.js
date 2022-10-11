f04431c8154ff3aeb720ed8052e0cd7796ae794a
Aaron Albright <aalbright@politico.com>
2022-10-11 14:53:51 -0400

    backup fixes

--- src/actions/backup/index.js
+++ src/actions/backup/index.js
@@ -1,18 +1,19 @@
 /* eslint-disable import/prefer-default-export */
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { readBinaryFile, readDir } from '@tauri-apps/api/fs';
-import { join, homeDir, resolve } from '@tauri-apps/api/path';
+import { join, resolve } from '@tauri-apps/api/path';
 import { PROJECTS_ARCHIVE_PREFIX } from '../../constants/paths';
+import { getStoreValue, updateProject } from '../../store';
 
 /**
  * By default, will upload all .ai files found in a project folder
  * to the S3 staging bucket
+ * @param {String} projectName Project slug for directory path
  * @param {Object} opts
- * @param {String} project Project slug for directory path
- * @param {String[]} opts.files Adobe Illustartor file names
+ * @param {String[]} [opts.files] Adobe Illustartor file names
+ * WITHOUT the extension (e.g. `['illo-one', 'illo-two']`)
  */
 async function backupFilesS3(projectName, { files } = {}) {
-  const homeDirPath = await homeDir();
   const s3 = new S3Client({
     region: 'us-east-1',
     credentials: {
@@ -21,20 +22,28 @@ async function backupFilesS3(projectName, { files } = {}) {
     },
   });
 
-  const projectPath = await resolve(
-    homeDirPath,
-    'Artisan/Projects',
-    projectName,
-  );
+  const projectsFolderPath = await getStoreValue('projectsFolder');
+
+  // Path to project folder
+  const projectPath = await resolve(projectsFolderPath, projectName);
+
   const projectFiles = await readDir(projectPath);
 
+  // Removes .DS_store and any hidden files
+  // Returns array of illustration names without extensions
+  const fileNames = projectFiles
+    .filter(({ name }) => name.substring(0, 1) !== '.')
+    .map((d) => d.name);
+
   const handleUpload = async (file) => {
-    const projectFilePath = await join(projectName, file);
-    const filePath = await join(homeDirPath, 'Artisan/Projects', projectPath);
+    const filePath = await join(projectPath, file, `${file}.ai`);
     const content = await readBinaryFile(filePath);
 
-    const keyPath = await join(PROJECTS_ARCHIVE_PREFIX, projectPath);
-    console.log('Uploading to: ', keyPath);
+    const keyPath = await join(
+      PROJECTS_ARCHIVE_PREFIX,
+      projectName,
+      `${file}.ai`,
+    );
 
     try {
       /**
@@ -47,18 +56,25 @@ async function backupFilesS3(projectName, { files } = {}) {
         StorageClass: 'STANDARD',
       };
       const uploadCommand = new PutObjectCommand(params);
-      await s3.send(uploadCommand);
+      return await s3.send(uploadCommand);
     } catch (error) {
       console.error(error);
+      return null;
     }
   };
 
   try {
-    if (files) {
-      await Promise.all(files.map(handleUpload));
-    }
+    // Use either manually passed file names or all in directory
+    const filesToUpload = files || fileNames;
+
+    await Promise.all(filesToUpload.map(handleUpload));
+    return await updateProject(projectName, {
+      isUploaded: true,
+      lastUploaded: new Date().toISOString(),
+    });
   } catch (error) {
     console.error(error);
+    return null;
   }
 }
 
