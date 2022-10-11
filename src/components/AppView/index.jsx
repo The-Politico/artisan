84c4c72578bb605a1322b83e04e8e0c9cade45c7
Aaron Albright <aalbright@politico.com>
2022-10-11 14:54:20 -0400

    testing components

--- src/components/AppView/index.jsx
+++ src/components/AppView/index.jsx
@@ -1,37 +1,52 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import { ServerIcon } from '@heroicons/react/24/solid';
+import { useEffect, useState } from 'react';
 import Button from '../Button';
 import cls from './AppView.module.scss';
 import { backupFilesS3 } from '../../actions/backup';
 import SetFolder from '../SetFolder';
 import { getProjects } from '../../actions/get-projects-archive';
 import { deleteProject } from '../../actions/delete-project';
+import { removeProject, getStoreValue } from '../../store';
+import { store, projects } from '../../store/init';
+import { archiveProject } from '../../actions/archive';
 
 export default function AppView() {
-  const handleClick = () => {
-    backupFilesS3({
-      project: 'project-three',
-      files: ['test-one.ai'],
-    });
+  const [p, setP] = useState([]);
+
+  const handleClick = async () => {
+    const projectInfo = await backupFilesS3('project-three');
+    console.log(projectInfo);
   };
 
-  const listArchive = async () => {
-    const p = await getProjects();
-    console.log({ p });
+  const reset = async () => {
+    await store.reset();
+    await projects.reset();
+    // console.log({ pr });
   };
 
-  const deleteProj = () => {
-    deleteProject('project-one');
+  const deleteProj = async () => {
+    // await removeProject('project-five');
+    await archiveProject('project-five');
   };
+
+  useEffect(() => {
+    const listProjects = async () => {
+      const ps = await getStoreValue('projects');
+      setP(ps);
+    };
+    listProjects();
+  });
 
   return (
     <div className={cls.view}>
+      {p && p.map((d) => (<span key={d}>{d}</span>))}
       <SetFolder />
       <Button
-        onClick={listArchive}
-        variant="outline"
+        onClick={reset}
+        variant="solid"
       >
-        List archive
+        Reset
       </Button>
       <Button
         onClick={deleteProj}
--- src/components/SetFolder/SetFolder.jsx
+++ src/components/SetFolder/SetFolder.jsx
@@ -1,28 +1,24 @@
-import { Store } from 'tauri-plugin-store-api';
 import { resolve, homeDir } from '@tauri-apps/api/path';
+import { addProject, getStoreValue, updateAppSettings } from '../../store';
 import Button from '../Button';
-
-const store = new Store('.settings');
 
 async function handleClick() {
   const home = await homeDir();
   const projectsPath = await resolve(home, 'Artisan', 'Projects');
-  await store.set(
-    'projectsFolder',
-    projectsPath,
+  await updateAppSettings(
+    {
+      projectsFolder: projectsPath,
+    },
   );
 }
 
-async function load() {
-  await store.load();
-}
-
-async function save() {
-  await store.save();
+async function setProject() {
+  const f = await addProject('Project Five');
+  console.log(f);
 }
 
 async function get() {
-  const f = await store.get('projectsFolder');
+  const f = await getStoreValue('projects');
   console.log(f);
 }
 
@@ -30,16 +26,7 @@ export default function SetFolder() {
   return (
     <>
       <Button onClick={handleClick}>Set folder</Button>
-      <Button
-        onClick={load}
-      >
-        Load folder
-      </Button>
-      <Button
-        onClick={save}
-      >
-        Save folder
-      </Button>
+      <Button onClick={setProject}>New project</Button>
       <Button
         onClick={get}
       >
