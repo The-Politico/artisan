361116f231df3eef51d8daf94e9dcd7e03e47669
Aaron Albright <aalbright@politico.com>
2022-10-11 14:53:58 -0400

    open in finder better api

--- src/actions/open-in-finder/index.js
+++ src/actions/open-in-finder/index.js
@@ -1,8 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 import { open } from '@tauri-apps/api/shell';
+import { resolve } from '@tauri-apps/api/path';
+import { getStoreValue } from '../../store';
 
-async function openInFinder(path) {
-  await open(path);
+/**
+ * Opens projet folder in Finder
+ * @param {String} projectName - Project slug name (e.g. `my-project-name`)
+ */
+async function openInFinder(projectName) {
+  const projectsFolder = await getStoreValue('projectsFolder');
+  const projectPath = await resolve(projectsFolder, projectName);
+  await open(projectPath);
 }
 
 export { openInFinder };
