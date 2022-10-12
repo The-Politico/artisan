3e23c7538396d016fb2d6fc06fd0bd9602500f31
Aaron Albright <aalbright@politico.com>
2022-10-12 16:25:35 -0400

    downloader improved

--- src/utils/download-from-s3.js
+++ src/utils/download-from-s3.js
@@ -1,22 +1,14 @@
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { createDir, writeBinaryFile } from '@tauri-apps/api/fs';
-import { join, basename } from '@tauri-apps/api/path';
 
 /**
  * Downloads files from an S3 bucket as a
- * `Unit8Array` written to a binary file.
+ * `Uint8Array` written to a binary file.
  * @param {Object} opts
  * @param {String} opts.bucket S3 bucket name.
  * @param {String} opts.key S3 key path to download file from.
- * @param {String} [opts.fileName] File name of both the
- * source and destination file. If no file name is provided, the
- * file name will be extracted from the S3 Key path base name.
- * @param {String} opts.destination File destination path.
+ * @return {Uint16Array}
  */
-export async function downloadS3Object({
-  bucket,
-  key,
-} = {}) {
+export async function downloadS3Object({ bucket, key } = {}) {
   const s3 = new S3Client({
     region: 'us-east-1',
     credentials: {
@@ -25,20 +17,22 @@ export async function downloadS3Object({
     },
   });
 
-  /**
-   * @type {import('@aws-sdk/client-s3').GetObjectCommandInput}
-   */
-  const inputParams = {
+  const command = new GetObjectCommand({
     Bucket: bucket,
     Key: key,
-  };
+  });
 
-  const command = new GetObjectCommand(inputParams);
-  const response = await s3.send(command);
+  try {
+    const { Body } = await s3.send(command);
 
-  const res = new Response(response.Body);
+    // eslint-disable-next-line no-undef
+    const res = new Response(Body);
 
-  const buffer = await res.arrayBuffer();
+    const buffer = await res.arrayBuffer();
 
-  return new Uint8Array(buffer);
+    return new Uint8Array(buffer);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
