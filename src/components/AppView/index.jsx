01c261859bc1f2816f0bd27329efc9920cba77e1
Aaron Albright <aalbright@politico.com>
2022-10-31 09:35:43 -0400

    added tab toggle

--- src/components/AppView/index.jsx
+++ src/components/AppView/index.jsx
@@ -1,17 +1,12 @@
 import cls from 'classnames';
-import { PlusIcon } from '@heroicons/react/20/solid';
-import { Menu, Tab } from '@headlessui/react';
+import { Menu } from '@headlessui/react';
 import { useState } from 'react';
 import styles from './styles.module.css';
-import { flex, spacing, typography as type, gap } from '../../theme';
-import IconButton from '../IconButton';
-import ProjectStatusIcon from '../ProjectStatusIcon';
-import ProjectStatusDek from '../ProjectStatusDek';
-import Button from '../Button';
+import { flex, spacing } from '../../theme';
 import MeatballItem from '../MeatballItem';
 import { getStoreValue } from '../../store';
-import TabToggleItem from '../TabToggleItem';
 import EmptyProject from '../EmptyProject';
+import TabToggle from '../TabToggle';
 
 export default function AppView() {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -41,57 +36,19 @@ export default function AppView() {
     );
   }
 
+  const tabItems = [
+    { iconName: 'DocumentIcon', mode: 'local' },
+    { mode: 'archive', iconName: 'ArchiveBoxIcon' },
+  ];
+
+  const screenSizes = [
+    { iconName: 'ComputerDesktopIcon', mode: 'desktop' },
+    { iconName: 'DevicePhoneMobileIcon', mode: 'mobile' },
+    { iconName: 'DeviceTabletIcon', mode: 'tablet' },
+  ];
+
   return (
     <div className={classNames}>
-      <div className={cls(flex.flex, spacing.x4)}>
-        <Button variant="ghost">Ghost button</Button>
-        <Button variant="outline">Outline button</Button>
-        <Button
-          variant="solid"
-          value="Solid w/ icon"
-          className={type.textXl}
-          icon={<PlusIcon className={styles.icon} />}
-        />
-      </div>
-      <div className={cls(flex.flex, flex.flexCenter, gap.x4)}>
-        <div className={styles.groupBg}>
-          <IconButton
-            iconName="GlobeAltIcon"
-            label="Publish"
-            setWhite
-          />
-        </div>
-        <IconButton
-          iconName="EyeIcon"
-          label="Preview"
-        />
-      </div>
-      <div className={styles.statusIcons}>
-        <ProjectStatusIcon />
-        <ProjectStatusIcon status="published" />
-        <ProjectStatusIcon status="archive" />
-        <ProjectStatusIcon size="lg" />
-        <ProjectStatusIcon
-          status="published"
-          size="lg"
-        />
-        <ProjectStatusIcon
-          status="archive"
-          size="lg"
-        />
-      </div>
-      <div>
-        <ProjectStatusDek />
-        <ProjectStatusDek status="archive" />
-        <ProjectStatusDek
-          status="published"
-          timestamp="2022-10-24T18:23:42.536Z"
-        />
-        <ProjectStatusDek
-          status="uploaded"
-          timestamp="2022-10-22T18:23:42.536Z"
-        />
-      </div>
       <Menu as="div">
         <MeatballItem
           iconName="ServerIcon"
@@ -116,48 +73,22 @@ export default function AppView() {
           danger
         />
       </Menu>
-      <Tab.Group
+      <TabToggle
         selectedIndex={selectedIndex}
-        onChange={setSelectedIndex}
-      >
-        <Tab.List className={cls(styles.tabList)}>
-          <TabToggleItem iconName="DocumentIcon" />
-          <TabToggleItem iconName="ArchiveBoxIcon" />
-          <span
-            style={{
-              '--translate': selectedIndex,
-            }}
-            className={cls(styles.glider)}
-          />
-        </Tab.List>
-      </Tab.Group>
-      <Tab.Group
-        selectedIndex={selectedIndex2}
-        onChange={setSelectedIndex2}
-        as="div"
-        className={styles.groupBg}
-      >
-        <Tab.List className={cls(styles.tabList, styles.transparentBg)}>
-          <TabToggleItem
-            size="24"
-            iconName="ComputerDesktopIcon"
-          />
-          <TabToggleItem
-            size="24"
-            iconName="DevicePhoneMobileIcon"
-          />
-          <TabToggleItem
-            size="24"
-            iconName="DeviceTabletIcon"
-          />
-          <span
-            style={{
-              '--translate': selectedIndex2,
-            }}
-            className={cls(styles.glider)}
-          />
-        </Tab.List>
-      </Tab.Group>
+        setSelectedIndex={setSelectedIndex}
+        items={tabItems.map(({ iconName }) => iconName)}
+      />
+      <p>{`Project list to show: ${tabItems[selectedIndex].mode}`}</p>
+      <div className={styles.groupBg}>
+        <TabToggle
+          selectedIndex={selectedIndex2}
+          setSelectedIndex={setSelectedIndex2}
+          items={screenSizes.map(({ iconName }) => iconName)}
+          size="24"
+          transparent
+        />
+      </div>
+      <p>{`Device selected: ${screenSizes[selectedIndex2].mode}`}</p>
     </div>
   );
 }
