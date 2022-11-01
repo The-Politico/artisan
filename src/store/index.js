d010e1cc13b62efe3949a7196725921c3f97374d
Aaron Albright <aalbright@politico.com>
2022-11-01 14:50:44 -0400

    more v2 changes

--- src/store/index.js
+++ src/store/index.js
@@ -14,49 +14,6 @@ import updatePreview from './operations/updatePreview';
 import updateProject from './operations/updateProject';
 import updateSettings from './operations/updateSettings';
 
-<<<<<<< HEAD
-/**
- * @typedef {import('./types').Key} Key
- */
-
-/**
- * Arbitrary convenience function to set a store value of a given key
- * Defaults to safe set only. Use `override: true` to unsafely set a new value
- * for any key.
- * @param {Key} key
- * @param {*} value
- * @param {Object} [opts]
- * @param {Boolean} [opts.override] Override safety check of existing key
- * @returns {Promise | null}
- */
-export async function updateStoreValue(key, value, { override = false } = {}) {
-  const hasKey = await store.has(key);
-  if (!hasKey || override) {
-    await store.set(key, value);
-    await store.save();
-    return store.get(key);
-  }
-  return null;
-}
-
-/**
- * Returns value for a given `key` or `null` if the key doesn't exist.
- * @param {Key} key
- * @returns {Promise<T | null>}
- */
-export async function getStoreValue(key) {
-  return store.get(key);
-}
-
-/**
- * Returns a project for a given `key` or `null` if the project doesn't exist.
- * @param {String} key
- * @returns {Promise<T | null>}
- */
-export async function getProject(key) {
-  return projects.get(key);
-}
-=======
 export default {
   addIllustration,
   addProject,
@@ -77,4 +34,3 @@ export default {
     PROJECTS,
   },
 };
->>>>>>> 8b476586ef1a17a2307ec3cdfaef13b0da35f1c3
