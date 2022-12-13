8a9435c1716f75df49341142576f7703940a5797
Aaron Albright <aalbright@politico.com>
2022-12-13 15:20:22 -0500

    PR fixes

--- src/components/SettingsPopover/Advanced.jsx
+++ src/components/SettingsPopover/Advanced.jsx
@@ -5,7 +5,6 @@ import {
   borders,
   colors,
   margin,
-  padding,
   typography as type,
 } from '../../theme';
 import Input from '../Input';
@@ -14,52 +13,49 @@ import styles from './styles.module.css';
 export default function Advanced({
   isInitial,
   projectsDir,
-  setProjestDir,
   port,
   setPort,
 }) {
-  if (!isInitial) {
-    return (
-      <Disclosure
-        as="div"
-        className={cls(styles.advanced)}
-      >
-        {({ open }) => (
-          <>
-            <Disclosure.Button
-              className={cls(
-                styles.buttonDropdown,
-                type.textSm,
-                type.fontSemibold,
-                colors.textSlate700,
-                margin.mb2,
-              )}
-            >
-              Advanced
-              <ChevronUpIcon
-                className={cls(styles.icon, { [styles.iconReverse]: open })}
-              />
-            </Disclosure.Button>
-            <Disclosure.Panel>
-              <Input
-                label="Preferred Port"
-                value={port}
-                setValue={setPort}
-                className={styles.settingsInput}
-              />
-
-              <p className={cls(type.textSm, colors.textSlate700, margin.mb2)}>
-                Projects Folder
-              </p>
-              <p className={cls(styles.dir, borders.roundedMd)}>
-                {projectsDir}
-              </p>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
-    );
+  if (isInitial) {
+    return null;
   }
 
-  return <div />;
+  return (
+    <Disclosure
+      as="div"
+      className={cls(styles.advanced)}
+    >
+      {({ open }) => (
+        <>
+          <Disclosure.Button
+            className={cls(
+              styles.buttonDropdown,
+              type.textSm,
+              type.fontSemibold,
+              colors.textSlate700,
+              margin.mb2,
+            )}
+          >
+            Advanced
+            <ChevronUpIcon
+              className={cls(styles.icon, { [styles.iconReverse]: open })}
+            />
+          </Disclosure.Button>
+          <Disclosure.Panel>
+            <Input
+              label="Preferred Port"
+              value={port}
+              setValue={setPort}
+              className={styles.settingsInput}
+            />
+
+            <p className={cls(type.textSm, colors.textSlate700, margin.mb2)}>
+              Projects Folder
+            </p>
+            <p className={cls(styles.dir, borders.roundedMd)}>{projectsDir}</p>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
 }
