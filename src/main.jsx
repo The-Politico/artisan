c0c0b5e6cb8143f33eaa09db38ef29085c5646e4
Andrew Briz <briz.andrew@gmail.com>
2022-12-14 15:11:17 -0500

    Swaping back to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import ActionTestingView from './components/ActionTestingView';
+import AppView from './components/AppView';
 import './main.css';
 
 ReactDOM.createRoot(window.document.getElementById('root')).render(
   <React.StrictMode>
-    <ActionTestingView />
+    <AppView />
   </React.StrictMode>,
 );
