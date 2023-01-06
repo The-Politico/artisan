c88cc4d402d9a8b3e7cbba0221c29207e143e6d2
rashidakamal <rsk2161@columbia.edu>
2023-01-06 14:32:17 -0500

    recopying from cms out of superstition

--- src/components/IllustrationPreview/_constants/embedTypes/_asideStandard.js
+++ src/components/IllustrationPreview/_constants/embedTypes/_asideStandard.js
@@ -1,20 +1,21 @@
 export default function AsideStandard({ children, className }) {
   return `\
-<section class="page-content__row page-content__row--story main-section">
-    <div class="container container--story story-layout--fixed-fluid">
-        <div class="container__column container__column--story center-horizontally hide-under-medium"></div>
-            <div class="container__column container__column--story">
-            <div class="container__row container__row--story story-layout--fluid-fixed">
-                <div class="container__column container__column--story center-horizontally">
-                    <div class="story-text">
-                            <aside class="story-enhancement ${className}">
-                                ${children}
-                            </aside>
-                    </div>
-                </div>
-                <div class="container__column container__column--story center-vertically right-zone hide-under-small" aria-hidden="true"></div>
-            </div>
-        </div>
-    </div>
-</section>`;
+  <section class="page-content__row page-content__row--story main-section">
+  <div class="container container--story story-layout--fixed-fluid">
+  <div class="container__column container__column--story center-horizontally hide-under-medium"></div>
+  <div class="container__column container__column--story">
+  <div class="container__row container__row--story story-layout--fluid-fixed">
+  <div class="container__column container__column--story center-horizontally">
+  <div class="story-text">
+          <aside class="story-enhancement ${className}">
+          ${children}
+            </aside>
+                      </div>
+                  </div>
+  
+                  <div class="container__column container__column--story center-vertically right-zone hide-under-small" aria-hidden="true"></div>
+              </div>
+          </div>
+      </div>
+  </section>`;
 }
