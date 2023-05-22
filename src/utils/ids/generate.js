3bb7783d1196a6d2daedd2e2ba8dfcd2866771ba
Andrew Briz <briz.andrew@gmail.com>
2023-05-22 15:24:23 -0400

    Missed that one

--- src/utils/ids/generate.js
+++ src/utils/ids/generate.js
@@ -1,5 +1,5 @@
 import delimeter from './delimeter';
 
-export default function gen({ project, illustration }) {
+export default function generate({ project, illustration }) {
   return `${project}${delimeter}${illustration}`;
 }
