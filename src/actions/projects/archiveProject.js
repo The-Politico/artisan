2a0cdb48d46290380561769756b589fb178c3216
rashidakamal <rsk2161@columbia.edu>
2023-05-25 15:30:04 -0400

    checked actions

--- src/actions/projects/archiveProject.js
+++ src/actions/projects/archiveProject.js
@@ -1,6 +1,10 @@
 import { removeDir } from '@tauri-apps/api/fs';
 import getProjectPath from '../../utils/paths/getProjectPath';
 
+/**
+ * @deprecated
+*/
+
 export default async function archiveProject(id) {
   const projectPath = await getProjectPath(id);
 
