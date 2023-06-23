dea67b9e7bd25f1b5dec82abfdcda01df60868cf
Aaron Albright <aalbright@politico.com>
2023-06-23 15:25:27 -0400

    working locks

--- src/box-api/constants.js
+++ src/box-api/constants.js
@@ -1,5 +1,6 @@
 export const BOX_BASE_API = 'https://api.box.com/2.0';
 export const BOX_FOLDER_API = (folderId) => `${BOX_BASE_API}/folders/${folderId}`;
 export const BOX_LIST_FOLDERS_API = (folderId) => `${BOX_FOLDER_API(folderId)}/items`;
+export const BOX_LOCK_FOLDER = `${BOX_BASE_API}/folder_locks`;
 
 export const PROJECTS_FOLDER_ID_DEV = '210417182704';
--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,3 +1,4 @@
+import { Body, fetch } from '@tauri-apps/api/http';
 import ProjectList from '../ProjectList';
 import ArtisanProject from '../ArtisanProject';
 import Sidebar from '../Sidebar';
@@ -7,20 +8,71 @@ import { useActivateTime } from '../../atoms/now/init';
 import styles from './styles.module.css';
 import Button from '../Button';
 import atoms from '../../atoms';
+import { BOX_FOLDER_API, BOX_LOCK_FOLDER } from '../../box-api/constants';
 
 export default function AppView() {
   const [settings, setSettings] = atoms.useRecoilState(atoms.settings);
 
   useActivateTime();
 
+  const {
+    box_tokens: { access_token: token },
+  } = settings;
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+
   // log settings to confirm tokens were added
   const handleClick2 = async () => {
     console.log(settings);
   };
 
+  const lockFolder = async () => {
+    const d = await fetch(BOX_FOLDER_API('213943980371'), {
+      method: 'GET',
+      headers,
+    });
+    console.log(d);
+    const r = await fetch(BOX_LOCK_FOLDER, {
+      headers,
+      method: 'POST',
+      body: Body.json({
+        folder: {
+          id: '213943980371',
+          type: 'folder',
+        },
+      }),
+    });
+
+    console.log(r);
+  };
+
+  const deleteLock = async () => {
+    const locks = await fetch(`${BOX_LOCK_FOLDER}?folder_id=213943980371`, {
+      method: 'GET',
+      headers,
+    });
+    console.log(locks.data);
+    // const r = await fetch(`${BOX_LOCK_FOLDER}/2805965804`, {
+    //   headers,
+    //   method: 'DELETE',
+    // });
+    // console.log(r.data);
+  };
+
   return (
     <div className={styles.grid}>
       <Sidebar>
+        <Button
+          onClick={lockFolder}
+          value="Lock folder"
+        />
+        <br />
+        <Button
+          onClick={deleteLock}
+          value="Delete lock"
+        />
+        <br />
         <Button
           onClick={handleClick2}
           value="log settings"
