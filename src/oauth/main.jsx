0b75da10d8c1b1e55bede3e205108790e78df1e1
Aaron Albright <aalbright@politico.com>
2023-06-27 12:52:46 -0400

    remvoe recoil from oauth

--- src/oauth/main.jsx
+++ src/oauth/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
 import OathConfirm from './OathConfirm';
 
 import '../main.css';
 
 ReactDOM.createRoot(window.document.getElementById('root')).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <OathConfirm />
-    </RecoilRoot>
+    <OathConfirm />
   </React.StrictMode>,
 );
