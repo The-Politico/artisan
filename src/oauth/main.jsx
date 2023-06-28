daafeb477a57e55732f7228358ed5903be7387c0
Aaron Albright <aalbright@politico.com>
2023-06-28 13:49:02 -0400

    file rename

--- src/oauth/main.jsx
+++ src/oauth/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import OathConfirm from './OathConfirm';
+import OauthConfirm from './OauthConfirm';
 
 import '../main.css';
 
 ReactDOM.createRoot(window.document.getElementById('root')).render(
   <React.StrictMode>
-    <OathConfirm />
+    <OauthConfirm />
   </React.StrictMode>,
 );
